520e203e2d7aa558a425fde95072dc4184ee9f15
Priscylla <Priscylla.Delta@gmail.com>
2024-11-08 19:11:34 -0600

    Add files via upload
    Moved Everything to functions and created a loop till the user is done with the program (Still no Storage between uses)

--- notes.py
+++ notes.py
@@ -1,26 +1,39 @@
 
 
+
 notes = {}
 
-options = ["Add", "Update", "Delete", "Print"]
+def main():
+    finished = False
+    while finished == False:
+        action = get_Action()
 
-selection_phrase = "Select from the available options Below \n'Add', 'Update', 'Delete', 'Print' \n: "
-finished = False
-while finished == False:
-    selection = input(selection_phrase)
+        if action == "Add":
+            add_Notes()
     
+        finished = check_Done(finished)
+
+
+
+def get_Action():
+    options = ["Add", "Update", "Delete", "Print"]
+    selection_phrase = "Select from the available options Below \n'Add', 'Update', 'Delete', 'Print' \n: "
     
-    for option in options:
-        if selection.lower() == option.lower():
-            finished = True
-            selection = option
-            print(f"'{selection}' Selected")
-        if selection not in options:
-            selection_phrase = "Option You've Selected is either not in the expected list or not up to date, please try again"
+    valid_option = False
+    while valid_option == False:
+        selection = input(selection_phrase)
+        
+        for option in options:
+            if selection.lower() == option.lower():
+                valid_option = True
+                selection = option
+                print(f"'{selection}' Selected")
+            if selection not in options:
+                selection_phrase = "Option You've Selected is either not in the expected list or not up to date, please try again"
 
+    return selection
 
-
-if selection == "Add":
+def add_Notes():
     title = input("What is the title of the note you'd like to add?\n: ")
     message = input("What is the note you'd like to write?\n: ")
 
@@ -29,9 +42,31 @@ if selection == "Add":
     print(f"{title} : {message}")
 
 
-print("Done")
-input()
+def check_Done(finished):
+    options = ["Yes", "Y", "No", "N"]
+    selection_phrase = "Are you Finished? ('Yes', 'Y', 'No', 'N')\n: "
+    valid_option = False
+    while valid_option == False:
+        selection = input(selection_phrase)
+        
+        for option in options:
+            if selection.lower() == option.lower():
+                valid_option = True
+                selection = option
 
-## Add
-## Update
-## Delete
+    if selection == "Yes" or selection == "Y":
+        finished = True
+
+    return finished
+
+
+
+
+
+
+
+
+
+
+
+main()
