beb15be7f09f7b9ef4b39c2e48bab4f87f3c1b55
Priscylla <Priscylla.Delta@gmail.com>
2024-11-08 20:16:46 -0600

    Added Reading Notes and storage
    Notes are stored in a csv, can be added to or read, next is updating

--- notes.py
+++ notes.py
@@ -1,7 +1,5 @@
+import csv
 
-
-
-notes = {}
 
 def main():
     finished = False
@@ -10,14 +8,15 @@ def main():
 
         if action == "Add":
             add_Notes()
+        if action == "Read":
+            print_Note()
     
         finished = check_Done(finished)
 
 
-
 def get_Action():
-    options = ["Add", "Update", "Delete", "Print"]
-    selection_phrase = "Select from the available options Below \n'Add', 'Update', 'Delete', 'Print' \n: "
+    options = ["Add", "Update", "Delete", "Read"]
+    selection_phrase = "Select from the available options Below \n'Add', 'Update', 'Delete', 'Read' \n: "
     
     valid_option = False
     while valid_option == False:
@@ -33,13 +32,47 @@ def get_Action():
 
     return selection
 
+
 def add_Notes():
     title = input("What is the title of the note you'd like to add?\n: ")
     message = input("What is the note you'd like to write?\n: ")
 
-    notes[title] = message
+    with open("Notes.csv", "a", newline="") as notes_File:
+        writer = csv.writer(notes_File, delimiter=',')
+        writer.writerow([title, message])
 
     print(f"{title} : {message}")
+
+    return
+
+
+def print_Note():
+    with open("Notes.csv", newline="") as notes_File:
+        reader = csv.DictReader(notes_File, delimiter=",")
+        notes = list(reader)
+
+    titles = []
+    for row in notes:
+        titles.append(row["Title"])
+        
+    selection_phrase = f"Select from the available notes below.\n{titles}\n"
+    valid_title = False
+    while valid_title == False:
+        selection = input(selection_phrase)
+        
+        for title in titles:
+            if selection.lower() == title.lower():
+                valid_title = True
+                selection = title
+                for row in notes:
+                    if row["Title"] == selection:
+                        print(row["Note"])
+
+
+            if selection not in title:
+                selection_phrase = "Note youve selected doesnt share a name with a Note already in the list, please try again"
+
+    return
 
 
 def check_Done(finished):
